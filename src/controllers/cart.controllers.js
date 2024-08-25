75a3cda1dc8a06def9333355cbe2e261395c5386
anghelomanini12 <vinces241015@gmail.com>
2024-08-25 03:08:59 -0500

    details 2

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -2,88 +2,87 @@ const catchError = require('../utils/catchError');
 const Cart = require('../models/Cart');
 const Product = require('../models/Product');
 const Category = require('../models/Category');
+const ProductImg = require('../models/ProductImg');
 
-const getAll = catchError(async (req, res) => {
-  const userId = req.user.id
-  const results = await Cart.findAll({
-    where: { userId },
-    include: [
-      {
-        model: Product,
-        attributes: { exclude: ['updatedAt', 'createdAt'] },
+
+const getAll = catchError(async(req, res) => {
+    const userId = req.user.id;
+    const results = await Cart.findAll({
+        where: { userId: userId },
         include: [
           {
-            model: Category,
-            attributes: ['name', 'id']
+            model: Product,
+            attributes: { exclude: ['createdAt', 'updatedAt'] },
+            include: [
+              {
+                model: Category,
+                attributes: ['name']
+              },
+              {
+                model: ProductImg,
+                attributes: ['url']
+              }
+            ]
           }
         ]
-      }
-    ]
-  });
-  return res.json(results);
+      });
+      return res.json(results);
 });
-
-// const create = catchError(async (req, res) => {
-//   const userId = req.user.id
-//   const result = await Cart.create({ ...req.body, userId });
-//   return res.status(201).json(result);
-// });
-
-const create = catchError(async (req, res) => {
-  const userId = req.user.id
-  const { productId, quantity } = req.body
-  const body = { productId, quantity, userId }
-  const result = await Cart.create(body);
-  return res.status(201).json(result);
+const create = catchError(async(req, res) => {
+    const userId = req.user.id;
+    const { quantity, productId } = req.body 
+  
+    const result = await Cart.create({ userId: userId, quantity: quantity, productId: productId });
+    return res.status(201).json(result);
 });
-
-
-const getOne = catchError(async (req, res) => {
-  const userId = req.user.id
-  const { id } = req.params;
-  const result = await Cart.findByPk(id, {
-    where: { userId },
-    include: [
-      {
-        model: Product,
-        attributes: { exclude: ['updatedAt', 'createdAt'] },
-        include: [
-          {
-            model: Category,
-            attributes: ['name', 'id']
-          }
-        ]
-      }
-    ]
-  });
-  if (!result) return res.sendStatus(404);
-  return res.json(result);
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const userId = req.user.id
+    const result = await Cart.findByPk(id, {
+      where: { userId: userId },
+      include: [
+        {
+          model: Product,
+          attributes: { exclude: ['createdAt', 'updatedAt'] },
+          include: [
+            {
+              model: Category,
+              attributes: ['name']
+            },
+            {
+              model: ProductImg
+            }
+          ]
+        }
+      ]
+    });
+    if (!result) return res.sendStatus(404);
+    return res.json(result);
 });
-
-const remove = catchError(async (req, res) => {
-  const userId = req.user.id
-  const { id } = req.params;
-  const result = await Cart.destroy({ where: { id, userId } });
-  if (!result) return res.sendStatus(404);
-  return res.sendStatus(204);
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    const userId = req.user.id;
+    const result = await Cart.destroy({ where: {id, userId} });
+    if(!result) return res.sendStatus(404);
+    return res.sendStatus(204);
 });
-
-const update = catchError(async (req, res) => {
-  const userId = req.user.id
-  const { id } = req.params;
-  const { quantity } = req.body
-  const result = await Cart.update(
-    { quantity },
-    { where: { id, userId }, returning: true }
-  );
-  if (result[0] === 0) return res.sendStatus(404);
-  return res.json(result[1][0]);
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+    delete req.body.userId;
+    delete req.body.productId;
+  
+    const result = await Cart.update(
+      req.body,
+      { where: { id, userId: req.user.id }, returning: true }
+    );
+    if (result[0] === 0) return res.sendStatus(404);
+    return res.json(result[1][0]);
+    
 });
-
 module.exports = {
-  getAll,
-  create,
-  getOne,
-  remove,
-  update
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
 }

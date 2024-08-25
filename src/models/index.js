7fa05dc7bd022318abbc76b6b060fbb9581e73d2
anghelomanini12 <vinces241015@gmail.com>
2024-08-24 22:11:03 -0500

    models2

---
+++ src/models/index.js
@@ -0,0 +1,27 @@
+const Cart = require("./Cart")
+const Category = require("./Category")
+const Product = require("./Product")
+const Purchase = require("./Purchase")
+const User = require("./User")
+
+// Product --> categoryId
+Product.belongsTo(Category)
+Category.hasMany(Product)
+
+//Cart -> userId
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+//Cart -> productId
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
+
+
+//Cart -> userId
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+//Cart -> productId
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
+
